ca68106cd7d32bc0bb45d57d8cfe6ea67eacc436
jobas <101352388+jobas5@users.noreply.github.com>
2023-11-06 15:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,72 @@
-import numpy as np
 import streamlit as st
 import pickle as pkl
+import numpy as np
 
 
-if __name__ == '__main__':
-    st.title('Aplikasi Churn Analysis :writing_hand:')
+
+class MultiPage:
+
+    def __init__(self):
+        self.pages = []
+
+
+    def add_page(self, title, func):
+        self.pages.append({
+            "title":title,
+            "function":func
+        })
+
+    def run(self):
+        page = st.sidebar.selectbox(
+            'Pages',
+            self.pages,
+            format_func=lambda page: page['title']
+        )
+
+        page['function']()
+
+
+multi_page = MultiPage()
+
+def page_one():
+    st.title('Home')
+
+
+def page_two():
+    st.title('Dashboard')
+    html_code = """
+    <style>
+    .responsive-iframe {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%; /* 16:9 aspect ratio (h/w) */
+    }
+    .responsive-iframe iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    </style>
+    <div class="responsive-iframe">
+        <iframe width="100%" height="100%" src="https://lookerstudio.google.com/embed/reporting/e250424c-d5da-4da0-bfb5-fb7607becac0/page/p_383q79vcbd" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    """
+
+    st.markdown(html_code, unsafe_allow_html=True)
+
+
+def page_three():
+    st.title('Churn Prediction :writing_hand:')
 
     with st.form('Feature Audio Analysis'):
         st.write('Input Features')
         tenure_month = st.number_input('Tenure Month: ', value=0, step=1)
-        location = st.number_input('Location: ', value=0, step=1)
+        location = st.radio(
+            "Lokasi User", ("Jakarta", "Bandung")
+        )
         device_class = st.number_input('Device Class: ', value=0, step=1)
         game = st.number_input('Game Product: ', value=0, step=1)
         music = st.number_input('Music Product: ', value=0, step=1)
@@ -29,3 +86,10 @@ if __name__ == '__main__':
 
             prediction = loaded_model.predict(data)
             st.write(f"Prediction: {prediction[0]}")
+
+
+multi_page.add_page('Home', page_one)
+multi_page.add_page('Dashboard', page_two)
+multi_page.add_page('Churn Prediction', page_three)
+
+multi_page.run()
