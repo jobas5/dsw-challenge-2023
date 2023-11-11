76a634d2b15663b728224bad17c4b5e1400e124b
jobas <101352388+jobas5@users.noreply.github.com>
2023-11-11 10:42:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,30 @@ class MultiPage:
             format_func=lambda page: page['title']
         )
 
+        st.markdown(
+            """
+            <style>
+            .stApp {
+                max-width: 100%;
+                margin: 0 auto;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
         page['function']()
 
 
 multi_page = MultiPage()
+
+def responsive_iframe(url):
+    code = f"""
+    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+        <iframe src="{url}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div>
+    """
+    st.markdown(code, unsafe_allow_html=True)
 
 def page_one():
     st.title('Home')
@@ -34,47 +54,52 @@ def page_one():
 
 def page_two():
     st.title('Dashboard')
-    html_code = """
-    <style>
-    .responsive-iframe {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding-bottom: 56.25%; /* 16:9 aspect ratio (h/w) */
-    }
-    .responsive-iframe iframe {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-    </style>
-    <div class="responsive-iframe">
-        <iframe width="100%" height="100%" src="https://lookerstudio.google.com/embed/reporting/e250424c-d5da-4da0-bfb5-fb7607becac0/page/p_383q79vcbd" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
-    """
-
-    st.markdown(html_code, unsafe_allow_html=True)
+    responsive_iframe("https://lookerstudio.google.com/embed/reporting/e250424c-d5da-4da0-bfb5-fb7607becac0/page/p_383q79vcbd&embed=0")
 
 
 def page_three():
     st.title('Churn Prediction :writing_hand:')
 
+    mapping = {
+        "Jakarta" : 0,
+        "Bandung" : 1,
+        "Low End" : 0,
+        "Mid End" : 1,
+        "High End": 2,
+        "No": 0,
+        "Yes": 1,
+        "No Internet Service": 2,
+        "Debit": 0,
+        "Pulsa": 1,
+        "Digital Wallet": 2,
+        "Credit": 3
+    }
+
+    prediction_mapping = {
+        0: "Non Churn",
+        1: "Churn"
+    }
+
     with st.form('Feature Audio Analysis'):
         st.write('Input Features')
         tenure_month = st.number_input('Tenure Month: ', value=0, step=1)
-        location = st.radio(
-            "Lokasi User", ("Jakarta", "Bandung")
-        )
-        device_class = st.number_input('Device Class: ', value=0, step=1)
-        game = st.number_input('Game Product: ', value=0, step=1)
-        music = st.number_input('Music Product: ', value=0, step=1)
-        education = st.number_input('Education: ', value=0, step=1)
-        call_center = st.number_input('Call Center: ', value=0, step=1)
-        payment_method = st.number_input('Payment Method: ', value=0, step=1)
+        location = st.radio("Lokasi User", ("Jakarta", "Bandung"))
+        device_class = st.radio('Jenis Device: ', ("Low End", "Mid End", "High End"))
+        game = st.radio('Berlangganan Produk Game: ', ("No", "Yes", "No Internet Service"))
+        music = st.radio('Berlangganan Produk Music: ', ("No", "Yes", "No Internet Service"))
+        education = st.radio('Berlangganan Produk Pendidikan: ', ("No", "Yes", "No Internet Service"))
+        call_center = st.radio('Call Center: ', ("No", "Yes"))
+        payment_method = st.radio('Metode Pembayaran: ', ("Debit", "Pulsa", "Digital Wallet", "Credit"))
         monthly_purchase = st.number_input('Monthly Purchase: ', value=0, step=1)
         cltv = st.number_input('CLTV: ', value=0, step=1)
+
+        location = mapping[location]
+        device_class = mapping[device_class]
+        game = mapping[game]
+        music = mapping[music]
+        education = mapping[education]
+        call_center = mapping[call_center]
+        payment_method = mapping[payment_method]
 
         data = np.array([tenure_month, location, device_class, game, music, education, call_center, payment_method, monthly_purchase, cltv]).reshape(1, -1)
 
@@ -85,7 +110,8 @@ def page_three():
                 loaded_model = pkl.load(file)
 
             prediction = loaded_model.predict(data)
-            st.write(f"Prediction: {prediction[0]}")
+            prediction_result = prediction_mapping[prediction[0]]
+            st.write(f"Prediction: {prediction_result}")
 
 
 multi_page.add_page('Home', page_one)
